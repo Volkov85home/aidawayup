7314c34d67ec00c56eb9645b62ca91e8ab036bfe
Aidawayup <31045489+Aidawayup@users.noreply.github.com>
2017-08-16 00:16:25 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+				$(document).ready(function(){
+				// Фикмированная шапка при скролле
+				$("#header").removeClass("default");
+				$(window).scroll(function(){
+					if ($(this).scrollTop() > 20) {
+						$("#header").addClass("default").fadeIn('fast');
+					} else {
+						$("#header").removeClass("default").fadeIn('fast');
+					};
+				});
+				// Плавный скролл по якорям
+				$('a[href^="#"]').click(function () { 
+				    elementClick = $(this).attr("href");
+					destination = $(elementClick).offset().top;
+					if($.browser.safari){
+					$('body').animate( { scrollTop: destination }, 1000 );
+					} else {
+				    	$('html').animate( { scrollTop: destination }, 1000 );
+					}
+					return false;
+			   });
+			});
